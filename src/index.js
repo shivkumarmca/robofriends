76dbb1d1f9c3092f88e22ffd3bfff5d8b290caf9
Shiv Kumar <helloshivmca@gmail.com>
2021-10-20 11:06:39 +0530

    fetch details with props destructuring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Hello from './Hello'
+import Card from './Card';
 import 'tachyons'
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greetings={'Welcome, Shiv'}/>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
