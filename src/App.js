31e7f6874a0f6df01520c70a9a026b4d5b2b0b78
Shiv Kumar <helloshivmca@gmail.com>
2021-10-20 16:13:59 +0530

    Search fx() completed!!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots'
+import './App.css'
 
-const state = {
-  robots: robots,
-  searchField: ''
-}
+class App extends Component {
+    constructor() {
+      super()
+      this.state = {
+        robots: robots,
+        searchfield: ''
+      }
+    }
+  
+    onSearchChange = (event) => {
+      this.setState({ searchfield: event.target.value })
+    }
+  
+    render() {
+      const { robots, searchfield } = this.state;
+      const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      })
+      return (
+          <div className='tc'>
+             <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots} />            
+          </div>
+        );
+    }
+  }
+  
+  export default App;
 
-const App = () => {
-  return (
-    <div className="tc">
-        <h1>Robo Friends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
-    </div>
-  );
-}
 
-export default App;
+// const App = () => {
+//   return (
+//     <div className="tc">
+//         <h1>Robo Friends</h1>
+//         <SearchBox />
+//         <CardList robots={robots} />
+//     </div>
+//   );
+// }
+
+// export default App;
