d1fca63872f5817afba05e6f991b71328a08a67d
Shiv Kumar <helloshivmca@gmail.com>
2021-10-20 18:19:44 +0530

    Build and Published on GitHub Pages

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if(true){
-    throw new Error('Some Error Occurred!');
-  }
+  // if(true){
+  //   throw new Error('Some Error Occurred!');
+  // }
   return (
     <div>
       {
